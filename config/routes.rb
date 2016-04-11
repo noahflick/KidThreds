0f3cf9db3c3d01ac512bc39ce23db1ab7d6cbe21
Noah Flick <noahflick@gmail.com>
2016-04-11 16:41:38 -0700

    root and welcome/index added,

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   resources :users, except: [:index, :destroy]
 
   resources :sessions, only: [:new, :create, :destroy]
