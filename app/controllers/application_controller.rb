2894b313073cba3a5521c59d0368c8d69d307c45
Noah Flick <noahflick@gmail.com>
2016-04-14 11:59:12 -0700

    better current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= begin
+      User.find(session[:user_id]) if session[:user_id]
+    rescue
+      session[:user_id] = nil
+      nil
+    end
   end
 
   def authorize
